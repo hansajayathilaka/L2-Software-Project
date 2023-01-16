f2bce1dde9188e229a1c4bbdd497092cfdea89a3
Upeksha Herath <upekshadilshan000@gmail.com>
2023-01-16 20:25:39 +0530

    Completed header and creadentials component outline

--- Code/SSI-Wallet/ssi_mobile_wallet/App.js
+++ Code/SSI-Wallet/ssi_mobile_wallet/App.js
@@ -4,10 +4,12 @@ import SignIn from './src/components/SignIn';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Credentials from './src/components/Credentials';
+import Header from './src/components/Header';
+import Conections from './src/components/Conections';
 
 const App = () => {
   return (
-    <Credentials />
+    <Conections />
   );
 }
 
