7b484cf0c09565681251d083b0eff948993e0dfa
Hansa Jayathilaka <hansajayathilaka@yahoo.com>
2023-06-09 00:49:11 +0530

    remove single quote form jsx file

--- Code/NFT-Marketplace/pages/_app.js
+++ Code/NFT-Marketplace/pages/_app.js
@@ -239,7 +239,7 @@ function _App({Component, pageProps}) {
                         <div
                             className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
                         <h2 className="text-center text-white text-xl font-semibold">Loading...</h2>
-                        <p className="w-1/3 text-center text-white">This may take a few seconds, please don't close this
+                        <p className="w-1/3 text-center text-white">This may take a few seconds, please don&apos;t close this
                             page.</p>
                     </div>
                     :
