0583d3041cc010e6d97f77ffc57bdbb22c9eb12d
Binari Dissanayake <91403558+BinuMDX@users.noreply.github.com>
2023-06-09 10:46:22 +0530

    add recent changes

--- Code/NFT-Minter/nft_minter_service/local_settings.example.py
+++ Code/NFT-Minter/nft_minter_service/local_settings.example.py
@@ -18,3 +18,13 @@ NFT_CONTRACT_ADDRESS = '0x0e506bFF5F0271F45a1aB41c22b695fB5bD20f39'
 INFURA_IPFS_PROJECT_ID = 'xxxxx...'
 INFURA_IPFS_PROJECT_SECRET = 'xxxxx...'
 INFURA_IPFS_URL = 'https://ipfs.infura.io:5001/api/v0/add'
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS =  True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
