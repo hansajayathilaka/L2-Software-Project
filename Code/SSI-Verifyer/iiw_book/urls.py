1599786f1615123eebbe4ddcb2df4d175bd7bea8
Hansa Jayathilaka <hansajayathilaka@yahoo.com>
2023-06-12 01:09:08 +0530

    Hot Fix: Remove unwanted url path from verifier

--- Code/SSI-Verifyer/iiw_book/urls.py
+++ Code/SSI-Verifyer/iiw_book/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path("verify/<str:connection_id>", views.verify, name="verify"),
     path("verified/", views.verified, name="verified"),
     path("in-progress/<str:connection_id>", views.in_progress, name="in_progress"),
-    path("backend", views.backend, name="backend"),
     path("webhooks/topic/<str:topic>/", views.webhooks, name="webhooks"),
 ]
