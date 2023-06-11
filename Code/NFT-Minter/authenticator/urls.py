db3940c4db304a83cae50e4313132cc88076173c
Binari Dissanayake <91403558+BinuMDX@users.noreply.github.com>
2023-06-11 17:30:17 +0530

    add validations and forget password changes

--- Code/NFT-Minter/authenticator/urls.py
+++ Code/NFT-Minter/authenticator/urls.py
@@ -1,16 +1,8 @@
 from django.urls import path
-
-from django.contrib.auth import views as auth_views
-
 from authenticator.views import LoginView
-from authenticator.views import resetPasswordView
+
 
 urlpatterns = [
     path('login', LoginView.as_view(), name='login'),
-    path('resetPassword', resetPasswordView.as_view(), name='resetPassword'), 
-    path('resetPasswordDone', auth_views.PasswordResetDoneView.as_view(template_name='resetPasswordSend.html',), name='password_reset_done'),    
-    path('resetPasswordConfirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('resetPasswordComplete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    
 ]
 
